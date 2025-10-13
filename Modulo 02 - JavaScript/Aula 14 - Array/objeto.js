259033e5c1e202ffbb3c8eec01853aa8662a90e9
Loren-1psum <Caioloren28@gmail.com>
2025-10-12 23:57:43 -0300

    A lot of updates

---
+++ Modulo 02 - JavaScript/Aula 14 - Array/objeto.js
@@ -0,0 +1,11 @@
+
+
+//Adicionar um item no objeto
+listaProdutos.armazenamento = "256 gb"
+produto['memoria-RAM'] = "8 gb"
+console.log(produto);
+
+//Remover um elemento
+delete produto.armazenamento
+delete produto['memoria-RAM']
+console.log(produto);
