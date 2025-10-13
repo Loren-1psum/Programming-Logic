064b8af98d019f7083507c51cd4ff284027c5a2b
Loren-1psum <Caioloren28@gmail.com>
2025-10-13 00:02:15 -0300

    Second batch of updates

--- Modulo 02 - JavaScript/Aula 12 - Eventos DOM e Condições/desafio/script02.js
+++ Modulo 02 - JavaScript/Aula 12 - Eventos DOM e Condições/desafio/script02.js
@@ -1,14 +1,7 @@
-let caixa = document.getElementById("mouse")
-
-function entradaMouse() {
-    caixa.style.backgroundColor = "blue"
+function mostrar() {
+    document.getElementById("texto").style.display = "block";
 }
 
-function saidaMouse() {
-    caixa.style.backgroundColor = "red"
-}
-
-function mudaHtml() {
-    let nome = document.querySelector("#nome").value
-    caixa.innerHTML = nome
+function esconder() {
+    document.getElementById("texto").style.display = "none";
 }
